8ddbf3c4b1fa325fa0d5101c8fc56b95903fbf17
Sander Haug <sander_haug@hotmail.com>
2022-12-15 15:24:47 +0100

    Initial Commit

---
+++ ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Sander Haug on 15/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
